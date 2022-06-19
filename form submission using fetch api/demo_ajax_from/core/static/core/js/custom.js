417fcbc752cfb4db1282a0f1271de91eb0a678d5
satyam-seth <satyam1998.1998@gmail.com>
2022-06-19 15:09:16 +0530

    update form submission using fetch api

--- form submission using fetch api/demo_ajax_from/core/static/core/js/custom.js
+++ form submission using fetch api/demo_ajax_from/core/static/core/js/custom.js
@@ -1,46 +1,45 @@
-const form=document.querySelector("form");
-console.log(form);
+window.onload = () => {
+    const form = document.querySelector("form");
+    console.log(form);
+    form.addEventListener("submit", submitForm);
+    async function submitForm(event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        console.log(formData);
 
-function submitForm(event){
-    event.preventDefault();
-    const data=new FormData(form);
-    console.log(data);
-
-    const params=new URLSearchParams();
-    for(pair of data){
-        params.append(pair[0],pair[1].toString());
-    }
-    console.log(params);
-
-    fetch('/', {method:'POST', body:params})
-    .then(response=>response.json())
-    .then((data)=>{
-        console.log(data)
-        
-        if(data.status==='done'){
-            form.reset()
-            document.querySelector(".message").innerText=data.message;
+        const params = new URLSearchParams();
+        for (pair of formData) {
+            params.append(pair[0], pair[1].toString());
         }
-        else if(data.status==='invalid'){
-            for(const error in data.errors){
-                console.log(error);
-                console.log(data.errors[error][0].message);
-    
-                const errorField=document.querySelector(`#id_${error}`);
-                errorField.focus();
-    
-                errorField.parentElement.classList.add('error-field')
-    
-                const helpSpan=document.querySelector(`#id_${error} + span.helptext`);
-                console.log(helpSpan);
-    
-                helpSpan.innerText=data.errors[error][0].message;
+        console.log(params);
+
+        try {
+            const res = await fetch('/', { method: 'POST', body: params });
+
+            if (res.status === 200) {
+                form.reset()
+                const data = await res.json();
+                document.querySelector(".message").innerText = data.message;
+            } else if (res.status === 400) {
+                const data = await res.json();
+                for (const error in data.errors) {
+                    console.log(error);
+                    console.log(data.errors[error][0].message);
+
+                    const errorField = document.querySelector(`#id_${error}`);
+                    errorField.focus();
+
+                    errorField.parentElement.classList.add('error-field')
+
+                    const helpSpan = document.querySelector(`#id_${error} + span.helptext`);
+                    console.log(helpSpan);
+
+                    helpSpan.innerText = data.errors[error][0].message;
+                }
             }
         }
-    })
-    .catch(error=>console.log(error))
-
-
+        catch (error) {
+            console.log(error);
+        }
+    }
 }
-
-form.addEventListener("submit", submitForm);
--- form submission using fetch api/demo_ajax_from/core/views.py
+++ form submission using fetch api/demo_ajax_from/core/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.urls import reverse 
+from django.urls import reverse
 from .forms import StudentForm
 
 # Create your views here.
+
 
 def home(request):
     if request.method == "POST":
@@ -11,8 +12,8 @@ def home(request):
         if form.is_valid():
             print(form.cleaned_data["roll_no"])
             print(form.cleaned_data["name"])
-            return JsonResponse({'status':'done','message':'form successfully submitted'})
-        return JsonResponse({'status':'invalid','errors':form.errors.get_json_data()})
+            return JsonResponse({'message': 'form successfully submitted'}, status=200)
+        return JsonResponse({'errors': form.errors.get_json_data()}, status=400)
     else:
         form = StudentForm()
-    return render(request, "core/home.html", {"form": form, 'action_url':reverse('home')})
+    return render(request, "core/home.html", {"form": form, 'action_url': reverse('home')})
