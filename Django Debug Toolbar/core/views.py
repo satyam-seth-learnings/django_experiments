264a2a285c0b2062b21aa4a770313d33df23c692
satyam-seth <satyam1998.1998@gmail.com>
2024-05-07 23:52:19 +0530

    add student list page

--- Django Debug Toolbar/core/views.py
+++ Django Debug Toolbar/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Student
 
 # Create your views here.
+
+
+class StudentListView(ListView):
+    """Student List View"""
+
+    model = Student
--- Django Debug Toolbar/demo/urls.py
+++ Django Debug Toolbar/demo/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("", views.StudentListView.as_view(), name="index"),
 ]
