6d2ceac3ff002e12e2a4b75272aba8121f307696
satyam-seth <satyam1998.1998@gmail.com>
2024-06-04 23:41:25 +0530

    add mail config for dev

--- Send Email in Django using Mailtrap/djangomailtrap/settings.py
+++ Send Email in Django using Mailtrap/djangomailtrap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email Testing only for Development
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "sandbox.smtp.mailtrap.io"
+EMAIL_HOST_USER = config("EMAIL_TEST_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_TEST_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_TEST_PORT")
