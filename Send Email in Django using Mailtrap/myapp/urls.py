5d4b5c4db808a3e11a44026683cf532018772c09
satyam-seth <satyam1998.1998@gmail.com>
2024-06-05 00:01:42 +0530

    add example to send plain text email message for dev env

--- Send Email in Django using Mailtrap/myapp/urls.py
+++ Send Email in Django using Mailtrap/myapp/urls.py
@@ -4,4 +4,5 @@ from myapp import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("send-test-email/", views.send_test_email, name="send_test_email"),
 ]
--- Send Email in Django using Mailtrap/myapp/views.py
+++ Send Email in Django using Mailtrap/myapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
     return render(request, "myapp/home.html")
+
+
+# Send Test Email (Dev)
+def send_test_email(self):
+    subject = "Test Email"
+    name = "User"
+    html_message = render_to_string("myapp/email_template.html", {"name": name})
+    plain_message = strip_tags(html_message)
+    from_email = "admin@example.com"
+    to_email = ["user@example.com"]
+
+    # Create email with Plain Text Message
+    email = EmailMessage(
+        subject=subject, body=plain_message, from_email=from_email, to=to_email
+    )
+
+    # Send the email
+    email.send()
+
+    return HttpResponse("Test email with Plain Message sent successfully.")
