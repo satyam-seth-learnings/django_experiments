35100f05fa22e9d78a56fdbaed23e59f25b2320a
Satyam Seth <satyam1998.1998@gmail.com>
2024-06-09 19:36:35 +0530

    regiter student model admin

--- Import Excel CSV Data to Database Using Pandas in Django/student/admin.py
+++ Import Excel CSV Data to Database Using Pandas in Django/student/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from student.models import Student
 
 # Register your models here.
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "age", "city"]
