91a31d96fdc55388be6ed2a0daa9d2cc0c54d65f
Satyam Seth <satyam1998.1998@gmail.com>
2024-06-09 22:29:50 +0530

    add DemoModel

--- Using faker package to dump database with dummy data/core/models.py
+++ Using faker package to dump database with dummy data/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DemoModel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
