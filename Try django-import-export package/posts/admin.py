74025a2e799a6cc342cee65735d8a2879cd14d4e
Satyam Seth <satyam1998.1998@gmail.com>
2024-06-12 23:37:15 +0530

    add post model import export mixins

--- Try django-import-export package/posts/admin.py
+++ Try django-import-export package/posts/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportActionModelAdmin, ImportExportMixin
 from .models import Post
 
 # Register your models here.
 
-
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(ImportExportActionModelAdmin, ImportExportMixin, admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'is_active', 'created_at')
