87c73139b7574cbc8e7feac873f685111942d771
Satyam Seth <satyam1998.1998@gmail.com>
2024-06-12 20:51:13 +0530

    create post model

--- Try django-import-export package/posts/models.py
+++ Try django-import-export package/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=220)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
