4ef599c4e2abd82cb5e1a22547822569a1eaae0d
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-07 00:13:11 +0530

    Fix settings

--- Send Email in Django using Mailtrap/djangomailtrap/settings.py
+++ Send Email in Django using Mailtrap/djangomailtrap/settings.py
@@ -133,11 +133,11 @@ EMAIL_HOST_PASSWORD = config("EMAIL_TEST_HOST_PASSWORD")
 EMAIL_PORT = config("EMAIL_TEST_PORT")
 
 
-# Email Testing only for Production (SMTP)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "live.smtp.mailtrap.io"
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+# # Email Testing only for Production (SMTP)
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "live.smtp.mailtrap.io"
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_PORT = config("EMAIL_PORT")
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
