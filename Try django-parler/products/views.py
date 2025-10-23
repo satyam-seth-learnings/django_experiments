0126037cb010bf726c6871d14fecfe698412a49b
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-23 23:10:05 +0530

    Add view for product details

--- Try django-parler/products/views.py
+++ Try django-parler/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+from django.utils.translation import get_language
 
-# Create your views here.
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    
+    # Fetch the translated fields based on the current language
+    translated_name = product.safe_translation_getter('name', language_code=get_language())
+    translated_description = product.safe_translation_getter('description', language_code=get_language())
+
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+        'name': translated_name,
+        'description': translated_description,
+    })
