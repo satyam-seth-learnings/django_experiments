764f1b8c1443c21f0abc79b2f06fb1076fcf918c
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-23 23:01:42 +0530

    Add product model

--- Try django-parler/products/models.py
+++ Try django-parler/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from parler.models import TranslatableModel, TranslatedFields
 
-# Create your models here.
+
+class Product(TranslatableModel):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    # Translated fields
+    translations = TranslatedFields(
+        name=models.CharField(max_length=255),
+        description=models.TextField(),
+    )
+
+    def __str__(self):
+        return self.name
