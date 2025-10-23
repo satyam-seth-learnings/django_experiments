2ad3a304b53bfee4ea7608274f1da367fb5dfa56
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-23 23:02:06 +0530

    Add product admin

--- Try django-parler/products/admin.py
+++ Try django-parler/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(TranslatableAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name', 'description')
