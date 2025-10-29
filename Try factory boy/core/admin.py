7937d8103a953348e1389055be9b3dcca6923ba7
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-29 23:47:41 +0530

    add admin for models

--- Try factory boy/core/admin.py
+++ Try factory boy/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'bio')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'publication_date')
+    list_filter = ('author', 'publication_date')
+    search_fields = ('title',)
