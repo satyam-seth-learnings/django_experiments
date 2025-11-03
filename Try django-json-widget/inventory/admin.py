99c0b264b594126886ed64b19757bbd55f047e13
Satyam Seth <satyam1998.1998@gmail.com>
2025-11-03 23:43:59 +0530

    Add model admin for item model

--- Try django-json-widget/inventory/admin.py
+++ Try django-json-widget/inventory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """Admin interface for Item model. without use of django-json-widget."""
+
+    list_display = ("name", "metadata")
