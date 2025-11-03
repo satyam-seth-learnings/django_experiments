4cccaadca7edfb7734425d7c2b170202678a1369
Satyam Seth <satyam1998.1998@gmail.com>
2025-11-03 23:42:55 +0530

    Add Item model

--- Try django-json-widget/inventory/models.py
+++ Try django-json-widget/inventory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    """Model representing an item with a name and metadata."""
+
+    name = models.CharField(max_length=100)
+    metadata = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        """Return the string representation of the item."""
+
+        return self.name
