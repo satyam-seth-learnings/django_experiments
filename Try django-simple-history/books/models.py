b6793a601fdc9789cb4ee69cc62775e8c6f889d1
Satyam Seth <satyam1998.1998@gmail.com>
2025-11-09 23:59:21 +0530

    Add history field to book model

--- Try django-simple-history/books/models.py
+++ Try django-simple-history/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 # Create your models here.
 
@@ -6,6 +7,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+
+    # Attach historical records to the model
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.title
