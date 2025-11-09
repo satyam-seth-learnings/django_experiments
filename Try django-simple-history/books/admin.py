270a7a728b931e0f66aa058c4ba2cf09628fa294
Satyam Seth <satyam1998.1998@gmail.com>
2025-11-10 00:05:22 +0530

    Register the Book model with SimpleHistoryAdmin for easy tracking in the admin panel

--- Try django-simple-history/books/admin.py
+++ Try django-simple-history/books/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import Book
 
 # Register your models here.
 
 
-admin.site.register(Book)
+# Register the Book model with SimpleHistoryAdmin for easy tracking in the admin panel
+admin.site.register(Book, SimpleHistoryAdmin)
