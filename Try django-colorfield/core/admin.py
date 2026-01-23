0dbb4a93db969742d39c6058b4bf86cce98e7610
Satyam Seth <satyam1998.1998@gmail.com>
2026-01-23 23:56:07 +0530

    Add admin for example model

--- Try django-colorfield/core/admin.py
+++ Try django-colorfield/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ExampleModel
 
 # Register your models here.
+
+@admin.register(ExampleModel)
+class ExampleModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "image",
+        "color",
+        "hex_color",
+        "hexa_color",
+        "rgb_color",
+        "rgba_color",
+        "restrictive_color",
+        "non_restrictive_color",
+    )
